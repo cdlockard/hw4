6466d169bb7bfe26de1876dab38e452cfa00066a
Colin Lockard <clockard@mills.edu>
2013-04-26 18:10:18 -0700

    ready to submit

--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe MoviesController do
 
 	describe 'edit director' do
-
+		#Colin added:
 		before :each do
 			@fake_movie = mock('movie1')
 			@fake_movie.stub(:title).and_return('fake')
@@ -12,37 +12,39 @@ describe MoviesController do
 		it 'should call the model method to update attributes' do
 			Movie.stub(:find).and_return(@fake_movie)
 			@fake_movie.should_receive(:update_attributes!).with("director" => 'Steven Spielberg')
-			put :update, {:movie => {:director => 'Steven Spielberg'}, :id=>@fake_movie.id}
+			put :update, {:movie => {:director => 'Steven Spielberg'}, :id=>@fake_movie.id} #performs the test
 		end
 
 
 	end
 
 	describe 'find movies with same director' do
+		#Colin added:
 		before :each do
 			@mov1=Movie.create!(:title => 'test1', :director => 'Steven Spielberg')
 		end
 		it 'should call Movie model method to find matching movies' do
 			movie_list = mock('movie list')
 			Movie.should_receive(:find_same_director).with(@mov1).and_return(movie_list)
-			get :find_by_director, {:id => @mov1.id}
+			get :find_by_director, {:id => @mov1.id} #performs the test
 		end
 
 		it 'should render the template for displaying matches' do
 			
 			get :find_by_director, {:id => '1'}
-			response.should render_template('find_by_director')
+			response.should render_template('find_by_director') #performs the test
 		end
 		it 'should make matching results available to the template' do
 			movie_list = mock('movie list')
 			Movie.stub(:find_same_director).and_return(movie_list)
 			get :find_by_director, {:id => '1'}
-			assigns(:movies).should == movie_list
+			assigns(:movies).should == movie_list#performs the test
 		end
 
 	end
 
 	describe 'fail to find movies by a null director' do
+		#Colin added:
 		before :each do
 			@mov1=Movie.create!(:title => 'test1')
 			@mov2=Movie.create!(:title => 'test2', :director => 'Federico Fellini')
@@ -52,7 +54,7 @@ describe MoviesController do
 		it 'should not call Movie model method to find matching movies' do
 			movie_list = mock('movie list')
 			Movie.should_not_receive(:find_same_director).with(@mov1).and_return(movie_list)
-			get :find_by_director, {:id => @mov1.id}
+			get :find_by_director, {:id => @mov1.id} #performs the test
 		end
 
 
